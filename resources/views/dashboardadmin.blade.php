d9f2d2f2d8e45c1120cfd5486546c0e272dfa1b7
0Faizal0 <faizal.10rpl1.2019@gmail.com>
2021-04-13 15:08:02 +0700

    dashboard admin update

--- resources/views/dashboardadmin.blade.php
+++ resources/views/dashboardadmin.blade.php
@@ -69,17 +69,52 @@
         <!-- MENU SIDEBAR-->
         <aside class="menu-sidebar d-none d-lg-block">
             <div class="logo">
-                <a href="#">
-                    <img src="images/icon/logo.png" alt="Cool Admin" />
-                </a>
+                <p style="font-weight: bold; color: #4fb9ff">Website Komunitas</p>
             </div>
-            <div class="menu-sidebar__content js-scrollbar1">
+            <div class="card">
                 <nav class="navbar-sidebar">
                     <ul class="list-unstyled navbar__list">
-                        <li class="active has-sub">
-                            <a class="js-arrow" href="#">
-                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        <li>
+                            <div class="account-wrap">
+                                <div class="account-item clearfix js-item-menu">
+                                    
+                                    
+                                        <div class="info clearfix">
+                                            <div class="image">
+                                                <a href="#" style="width:32px; height:32px; position:absolute; top:5px; left:2px; border-radius:50%">
+                                                    <img src="/uploads/avatars/default.jpg" alt="admin" />
+                                                </a>
+                                            </div>
+                                            <div class="content">
+                                                <h5 class="name">
+                                                    <a href="#">Admin</a>
+                                                </h5>
+                                                <span class="email" style="margin-left: 20%; font-size: 80%;">admin@gmail.com</span>
+                                            </div>
+                                        </div>
+                                        <div class="account-dropdown__body">
+                                            
+                                                <a href="#">
+                                                    <i class="zmdi zmdi-account"></i>Account</a>
+                                            </div>
+                                            
+                                                <a href="#">
+                                                    <i class="zmdi zmdi-settings"></i>Setting</a>
+                                            </div>
+                                            
+                                                <a href="#">
+                                                    <i class="zmdi zmdi-money-box"></i>Billing</a>
+                                            </div>
+                                            <div>
+                                                <a href="{{ route('logout') }}">
+                                                    <i class="zmdi zmdi-power"></i>Logout</a>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
                         </li>
+                        
                     </ul>
                 </nav>
             </div>
@@ -94,13 +129,13 @@
                     <div class="container-fluid">
                         <div class="header-wrap">
                             <form class="form-header" action="" method="POST">
-                                <input class="au-input au-input--xl" type="text" name="search" placeholder="Search for datas &amp; reports..." />
-                                <button class="au-btn--submit" type="submit">
+                                <input class="au-input au-input--xl" type="text" name="search" placeholder="Search for datas &amp; reports..." style="margin-top: 3%" />
+                                <button class="au-btn--submit" type="submit" style="margin-top: 3%">
                                     <i class="zmdi zmdi-search"></i>
                                 </button>
                             </form>
                             <div class="header-button">
-                                <div class="noti-wrap">
+                                <div class="noti-wrap" style="margin-top: 5%;">
                                     <div class="noti__item js-item-menu">
                                         <i class="zmdi zmdi-comment-more"></i>
                                         <span class="quantity">1</span>
@@ -214,24 +249,24 @@
                                 </div>
                                 <div class="account-wrap">
                                     <div class="account-item clearfix js-item-menu">
-                                        <div class="image">
-                                            <img src="images/icon/avatar-01.jpg" alt="John Doe" />
+                                        <div class="image" style="width:32px; height:32px; margin-top: 10%; border-radius:50%; margin-left: 10%;">
+                                            <img src="uploads/avatars/default.jpg" alt="admin" />
                                         </div>
-                                        <div class="content">
-                                            <a class="js-acc-btn" href="#">john doe</a>
+                                        <div class="content" style="margin-top: 5%;">
+                                            <a class="js-acc-btn" href="#" style="margin-top: 5%;">Admin</a>
                                         </div>
                                         <div class="account-dropdown js-dropdown">
                                             <div class="info clearfix">
                                                 <div class="image">
                                                     <a href="#">
-                                                        <img src="images/icon/avatar-01.jpg" alt="John Doe" />
+                                                        <img src="uploads/avatars/default.jpg" alt="admin" />
                                                     </a>
                                                 </div>
                                                 <div class="content">
                                                     <h5 class="name">
-                                                        <a href="#">john doe</a>
+                                                        <a href="#">Admin</a>
                                                     </h5>
-                                                    <span class="email">johndoe@example.com</span>
+                                                    <span class="email">admin@gmail.com</span>
                                                 </div>
                                             </div>
                                             <div class="account-dropdown__body">
@@ -268,63 +303,53 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-12">
-                
+                                <div class="info clearfix">
+                                    <div class="image">
+                                        <a href="#" style="width:32px; height:32px; position:absolute; left:5px; border-radius:50%; margin-left: 2%">
+                                            <img src="/uploads/avatars/dlogo.png" alt="admin" />
+                                        </a>
+                                    </div>
                                 <div class="overview-wrap">
-                                    <h2 class="title-1">overview</h2>
-                                    <button class="au-btn au-btn-icon au-btn--blue">
-                                        <i class="zmdi zmdi-plus"></i>add item</button>
+                                    <h2 class="title-1" style="margin-left: 5%; margin-right: 5%;">Dashboard</h2>
                                 </div>
                             </div>
                         </div>
-                        <div class="row m-t-25">
-                            <div class="col-sm-6 col-lg-4">
-                                <div class="overview-item overview-item--c1">
-                                    <div class="overview__inner">
-                                        <div class="overview-box clearfix">
-                                            <div class="icon">
+                        
+                                        <div class="overview-box clearfix" style="background-image: url('/uploads/avatars/menu1.png'); background-size: cover; margin-left: 10%; padding-top: 4%; padding-bottom: 4%; padding-left: 4%; padding-right: 4%; margin-top: 5%;">
+                                            <div class="icon" style="margin-top: 20%">
                                                 <i class="zmdi zmdi-account-o"></i>
                                             </div>
-                                            <div class="text">
+                                            <div class="text" style="margin-top: 20%">
                                                 <h2>10368</h2>
                                                 <span>Data Member</span>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
+                                    
 
-                            <div class="col-sm-6 col-lg-4">
-                                <div class="overview-item overview-item--c1">
-                                    <div class="overview__inner">
-                                        <div class="overview-box clearfix">
-                                            <div class="icon">
+                            
+                                        <div class="overview-box clearfix" style="background-image: url('/uploads/avatars/menu2.png'); background-size: cover; margin-left: 5%; padding-top: 4%; padding-bottom: 4%; padding-left: 4%; padding-right: 4%; margin-top: 5%;">
+                                            <div class="icon" style="margin-top: 20%">
                                                 <i class="zmdi zmdi-account-o"></i>
                                             </div>
-                                            <div class="text">
+                                            <div class="text" style="margin-top: 20%">
                                                 <h2>10368</h2>
                                                 <span>Pending User</span>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
+                                    
                 
-                            <div class="col-sm-6 col-lg-4">
-                                <div class="overview-item overview-item--c3">
-                                    <div class="overview__inner">
-                                        <div class="overview-box clearfix">
-                                            <div class="icon">
-                                                <i class="zmdi zmdi-calendar-note"></i>
+                            
+                                        <div class="overview-box clearfix" style="background-image: url('/uploads/avatars/menu3.png'); background-size: cover; margin-left: 5%; padding-top: 4%; padding-bottom: 4%; padding-left: 4%; padding-right: 4%; margin-top: 5%;">
+                                            <div class="icon" style="margin-top: 20%">
+                                                <i class="zmdi zmdi-account-o"></i>
                                             </div>
-                                            <div class="text">
+                                            <div class="text" style="margin-top: 20%">
                                                 <h2>20</h2>
                                                 <span>Daftar Group</span>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                        </div>
+                            
 
                         <style>
                             table.darkTable {
@@ -377,8 +402,8 @@
                             {{session('message')}}
                         </div>
                         @endif
-                        <table cellspacing="0" cellpadding="0" class="darkTable" style="margin-left:auto;margin-right:auto;">
-                            <tr style="background-color:#4d76b3; color:#ffffff;">
+                        <table cellspacing="0" cellpadding="0" class="darkTable" style="margin-left:auto;margin-right:auto; margin-top: 10%;">
+                            <tr style="background-color:#ffffff; color:#000000;">
                                 <td>Id</td>
                                 <td>Name</td>
                                 <td>Email</td>
